fa478195655df4dfa2c26351f91d9821ba077e07
Thiago Markendorf <thima3101@gmail.com>
2026-08-07 17:53:51 -0300

    Add project files.

---
+++ Distance/Distance/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿namespace Distance
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btCalcularDistancia_Clicked(object sender, EventArgs e)
+        {
+
+            double ax = Convert.ToDouble(Ax.Text);
+            double ay = Convert.ToDouble(Ay.Text);
+            double az = Convert.ToDouble(Az.Text);
+
+            double bx = Convert.ToDouble(Bx.Text);
+            double by = Convert.ToDouble(By.Text);
+            double bz = Convert.ToDouble(Bz.Text);
+
+            double distance = Math.Sqrt(Math.Pow(bx - ax, 2) + Math.Pow(by - ay, 2) + Math.Pow(bz - az, 2));
+
+            // lbResultado.Text = Convert.ToString(distance);
+            // lbResultado.Text = String.Format("{0:f2}", distance);
+            lbResultado.Text = $"{distance}:F4";
+
+        }
+
+    }
+}
